80a9c475dd73a1e5dd154fbe026d6ed4d8b5883d
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-20 10:02:57 +0000

    ajout du vimeo_player

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:vimeo_player_flutter/vimeo_player_flutter.dart';
 
 class HomeScrenn extends StatelessWidget {
   const HomeScrenn({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 250,
+          child: VimeoPlayer(
+            videoId: '707039256',
+          ),
+        ),
+      ),
+    );
   }
 }
