88cb1ddb9e186914ea2bf863cdae388332648737
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-20 19:50:59 +0000

    creation du Lesson screen

--- lib/screens/lesson_screen.dart
+++ lib/screens/lesson_screen.dart
@@ -1,28 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:irondev_api/widgets/vimeo_widget.dart';
 
-
-class HomeScrenn extends StatelessWidget {
-  const HomeScrenn({super.key});
+class LessonScreen extends StatelessWidget {
+  const LessonScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:Text('Iron Dev Academy', style: TextStyle(
-          color: Colors.white
-        ),),
+        title: Text(
+          'Iron Dev Academy',
+          style: TextStyle(color: Colors.white),
+        ),
         centerTitle: true,
         backgroundColor: Colors.blueAccent,
       ),
       body: Column(
         children: [
-           Container(
-              height: 250,
-              child: VimeoPlayer(
-                videoId: '707039256',
-              ),
+          Container(
+            height: 250,
+            child: VimeoPlayer(
+              videoId: '707039256',
             ),
+          ),
         ],
       ),
     );
