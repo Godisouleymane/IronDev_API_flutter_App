1581c8158dac624a6550b131c5735e6021fd0448
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-22 21:15:16 +0000

    creation du formation screen

---
+++ lib/screens/formations_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class FormationScreen extends StatelessWidget {
+  const FormationScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
