117deb7dd71cac1e6d8b061f13a063a2ae6ae57d
GodiSouleymane <ssabiougodi@gmail.com>
2024-02-22 21:23:28 +0000

    creation du formation screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,30 +1,32 @@
 import 'package:flutter/material.dart';
 import 'package:irondev_api/widgets/vimeo_widget.dart';
 
-
 class HomeScrenn extends StatelessWidget {
   const HomeScrenn({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:Text('Iron Dev Academy', style: TextStyle(
-          color: Colors.white
-        ),),
-        centerTitle: true,
-        backgroundColor: Colors.blueAccent,
-      ),
-      body: Column(
-        children: [
-           Container(
-              height: 250,
-              child: VimeoPlayer(
-                videoId: '707039256',
+        appBar: AppBar(
+          title: Text(
+            'Iron Dev Academy',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent,
+        ),
+        body: ListView(
+          children: const [
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.videocam),
+              title: Text('Leeson 1'),
+              subtitle: Text('Lesson 1'),
+              trailing: Icon(Icons.play_arrow),
               ),
-            ),
-        ],
-      ),
-    );
+              
+            )
+          ],
+        ));
   }
 }
